29418086998c86d7b46eaa4c6016261725f6943a
DuffMan2020 <57716152+DavidDTessier@users.noreply.github.com>
2021-05-13 08:42:31 -0400

    samples

---
+++ terraform/AWS/main.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_version = ">= 0.12, <=0.15"
+  required_providers {
+      aws = {
+          version = "~> 3.0",
+          source="hashicorp/aws"
+      }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "server_port" {
+    description = "The port the server will use for HTTP requests"
+    type = number
+    default = 8080
+}
+
+// create micro ec2 instance
+resource "aws_instance" "web_server" {
+    // Amazon Machine Image ID of an Ubuntu 18.04 AMI  in us-east-1
+    ami = "ami-09e67e426f25ce0d7" 
+    vpc_security_group_ids = [aws_security_group.web_server.id]
+    // EC2 Instance to run 
+    instance_type = "t2.micro"    
+    user_data = <<-EOF
+            #!/bin/bash
+            sudo yum update -y
+            sudo yum install httpd -y
+            sudo service httpd start
+            sudo chkconfig httpd on
+            echo "<html><h1>Welcome to AWS EC2 Instance deployed using Terraform!!!</h1></html>" | sudo tee /var/www/html/index.html
+            hostname -f >> /var/www/html/index.html
+            EOF
+    tags = {
+        Name = "terraform-webserver-example"
+    }
+}
+
+# Create security group with web and ssh access
+resource "aws_security_group" "web_server" {
+  name = "allow_web_traffic"
+
+ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "DNS" {
+  value = aws_instance.web_server.public_dns
+}
--- terraform/GCP/main.tf
+++ terraform/GCP/main.tf
@@ -1,3 +1,48 @@
+terraform {
+  required_version = ">= 0.12, <=0.15"
+  required_providers {
+      google = {
+          version = "~> 3.0",
+          source="hashicorp/google"
+      }
+  }
+}
+
+provider "google" {
+    project = var.project_id
+    region = var.region
+    credentials = file("credentials.json")
+}
+
+variable "project_id" {
+    description = "The GCP Project"
+    type = string
+}
+
+variable "region" {
+    description = "The region to deploy the webserver in."
+    type = string
+    default = "northamerica-northeast1"
+}
+
+variable "zone" {
+    description = "The zone associated to the region to provision the web server in"
+    type = string
+    default = "northamerica-northeast1-a"
+}
+
+variable "webservername" {
+    description = "The name to associate with the webserver"
+    type = string
+}
+
+variable "machine_type" {
+    description = "The machine type that will be used to create the vm."
+    type = string
+    default = "f1-micro"
+}
+
+
 data "google_compute_image" "debian" {
     family = "ubuntu-1804-lts"
     project = "ubuntu-os-cloud"
@@ -38,4 +83,8 @@ resource "google_compute_instance" "web-vm" {
     }
 
     metadata_startup_script = data.template_file.nginx.rendered
+}
+
+output "webserver_ip" {
+    value = google_compute_instance.web-vm.network_interface.0.access_config.0.nat_ip
 }
